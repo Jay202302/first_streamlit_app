edec5c312453e0a572ce40bd451439273dedfca1
Jay202302 <126977158+Jay202302@users.noreply.github.com>
2023-03-05 07:13:36 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Favourites')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('Avocado Toast')
+streamlit.header('Build Your  Own Fruit Smoothie')
