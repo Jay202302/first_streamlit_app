f1d0aa12c0f2eadca7a3b3fd6563a075c2cfcdfc
Jay202302 <126977158+Jay202302@users.noreply.github.com>
2023-03-05 07:46:59 +0000

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ import pandas
 # We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.fruit))
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
